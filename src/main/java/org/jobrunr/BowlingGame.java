de14f60d8fe97f1fde723551abcdc6e9a1839a5d
Dan-DH <daniel.diaz.hdez@gmail.com>
2026-01-09 11:23:57 +0100

    test solutions

--- src/main/java/org/jobrunr/BowlingGame.java
+++ src/main/java/org/jobrunr/BowlingGame.java
@@ -2,11 +2,64 @@ package org.jobrunr;
 
 public class BowlingGame {
 
+    private boolean strike = false;
+    private int strikeBonus = 0;
+    private boolean spare = false;
+    private int rollCount = 0;
+    private int pinsLeft = 10;
+    private int[] scoreArray = new int[12];
+
     public void roll(int pinsDown) {
-        throw new UnsupportedOperationException();
+        scoreArray[rollCount] += pinsDown;
+        pinsLeft -= pinsDown;
+
+        if (rollCount < 11) {
+            rollCount++;
+        }
+
+        if (strike) {
+            scoreArray[rollCount - 1] += pinsDown;
+            strikeBonus++;
+            pinsLeft = 10;
+            if (strikeBonus < 1) {
+                strike = false;
+                strikeBonus = 0;
+            }
+        }
+
+        if (spare) {
+            scoreArray[rollCount] += pinsDown;
+            spare = false;
+        }
+
+        if (pinsDown == 10) {
+            if (strike && rollCount < 9) {
+                scoreArray[rollCount - 1] += pinsDown;
+            }
+
+            strike = true;
+            strikeBonus = 0;
+            return;
+        }
+
+        if (pinsLeft == 0) {
+            spare = true;
+            pinsLeft = 10;
+            return;
+        }
+
+        if (rollCount % 2 == 0) {
+            pinsLeft = 10;
+        }
     }
 
     public int score() {
-        throw new UnsupportedOperationException();
+        int sc = 0;
+
+        for (int frame = 0; frame < scoreArray.length; frame++) {
+            sc += scoreArray[frame];
+        }
+
+        return sc;
     }
 }
